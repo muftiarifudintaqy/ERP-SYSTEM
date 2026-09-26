9e9a32c443b056f9f4cb43363d7d38f9ba02a254
Mufti Arifudin Taqy <muftiarifudintaqy0@gmail.com>
2026-09-26 10:37:55 +0700

    Login: Bootstrap, jQuery, ikon di-host sendiri

--- application/views/Template.php
+++ application/views/Template.php
@@ -12,14 +12,12 @@ if ($_SESSION['is_login']) {
   <meta name="description" content="We are Building Legacy, that Impactfull to the Society">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta charset="utf-8">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link href="<?= base_url() ?>assets/vendor/bootstrap/bootstrap.min.css" rel="stylesheet">
+  <script src="<?= base_url() ?>assets/vendor/bootstrap/bootstrap.bundle.min.js"></script>
   <link rel="shortcut icon" type="image/png" href="<?= base_url() ?>assets/img/fav.png">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link rel="preconnect" href="https://cdn.jsdelivr.net" crossorigin>
-  <link rel="preconnect" href="https://ajax.googleapis.com" crossorigin>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/vendor/bootstrap-icons/bootstrap-icons.css">
   <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css?v=1.0.1" type="text/css" media="screen" />
 
   <?php
@@ -34,7 +32,7 @@ if ($_SESSION['is_login']) {
   <meta property="og:image:width" content="1000" />
   <meta property="og:image:height" content="1000" />
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+  <script src="<?= base_url() ?>assets/vendor/jquery/jquery-3.6.1.min.js"></script>
   <link rel="stylesheet" href="<?= base_url() ?>assets/toast/jquery.toast.css">
   <script src="<?= base_url() ?>assets/toast/jquery.toast.js"></script>
 
